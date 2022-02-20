dc6f935ed760a92795f584c028ed38d5ddf63648
Michael Klassen <mklassen4124@gmail.com>
2022-02-20 11:21:15 -0800

    Game is now playable

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private int speed = 20;
+    private int speed = 30;
+    private bool movingRight;
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(-1f, 1f) * speed;
+        movingRight = (Random.value > 0.5);
+        Debug.Log(movingRight);
+        if (movingRight)
+        {
+            rb.velocity = new Vector2(Random.Range(1f,2f), 1f).normalized * speed;
+        }
+        else
+        {
+            rb.velocity = new Vector2(Random.Range(-1f, -2f), 1f).normalized * speed;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -20,11 +31,15 @@ public class BallMovement : MonoBehaviour
         {
             Debug.Log("Player 2 scored");
             rb.velocity = Vector2.zero;
+            GlobalVariables.player2Score++;
+            SceneManager.LoadScene("GameScene");
         }
         if (col.gameObject.tag == "Goal2")
         {
             Debug.Log("Player 1 scored");
             rb.velocity = Vector2.zero;
+            GlobalVariables.player1Score++;
+            SceneManager.LoadScene("GameScene");
         }
     }
 }
